1f4373b42699ea1d1a098be9a773da27a6db342e
karthikamarneni <karthikamarneni110223@gmail.com>
2025-03-04 22:05:41 +0530

    footer

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from "react-router-dom"
 import { Navbar } from "./Navbar"
+import { Footer } from "./Footer"
 
 export const Body = () => {
     return (
         <>
         <Navbar/>
         <Outlet/>
+        <Footer/>
         </>
     )
 }

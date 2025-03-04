0ce17b2fab64bfa3070f85107b90841918fc82ca
karthikamarneni <karthikamarneni110223@gmail.com>
2025-03-04 22:05:26 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { Navbar } from "./Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Body} from './Body';
+import {Login} from './Login';
+import { Profile } from "./Profile";
 
 
 
 function App() {
   return (
     <>
-    <Navbar/>
+    <BrowserRouter>
+    <Routes basename="/">
+    <Route path="/" element={<Body/>}>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+    </Route>
+     
+    </Routes>
+    </BrowserRouter>
+  
     </>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+    return (
+        <footer className="footer text-base-content p-4 fixed bottom-0">
+            <aside>
+                <p>
+                    Copyright © {new Date().getFullYear()} - All right reserved by ACME
+                    Industries Ltd
+                </p>
+            </aside>
+        </footer>
+    );
+};

f156d36ab29887d252d2e6b879b37e8ed287b103
karthikamarneni <karthikamarneni110223@gmail.com>
2025-03-05 07:33:02 +0530

    loginpage

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,74 +1,39 @@
+import { useState } from "react";
+import axios from 'axios';
+
 export const Login = () => {
+  const [emailId , setEmailId] = useState("karthik@gmail.com");
+  const [password , setPassword] = useState("Ammu@110223@")
+
+  const handleLogin = async() => {
+     try{
+      const data =  await axios.post('http://localhost:7001/login' , {email:emailId , password}, {withCredentials:true});
+      console.log(data)
+     }catch(err){
+      console.log(err.message)
+     }
+  }
   return (
     <div className="flex justify-center my-4" style={{ marginTop: "30px" }}>
-      <div className="card   shadow-xl bg-base-300" style={{width:"400px"}}>
+      <div className="card   shadow-xl bg-base-300" style={{ width: "400px" }}>
         <div className="card-body">
-          <h2 className="card-title">Login page!</h2>
-
-          <div className="card-actions flex flex-col">
-            <label className="input validator" style={{marginTop:'10px'}}>
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
-              </svg>
-              <input type="email" placeholder="mail@site.com" required />
-            </label>
-            <label className="input validator" style={{marginTop:'10px'}}>
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                  <circle
-                    cx="16.5"
-                    cy="7.5"
-                    r=".5"
-                    fill="currentColor"
-                  ></circle>
-                </g>
-              </svg>
-              <input
-                type="password"
-                required
-                placeholder="Password"
-                minlength="8"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-              />
-            </label>
-            <p className="validator-hint hidden">
-              Must be more than 8 characters, including
-              <br />
-              At least one number
-              <br />
-              At least one lowercase letter
-              <br />
-              At least one uppercase letter
-            </p>
-            <div className="validator-hint hidden">
-              Enter valid email address
+          <h2 className="card-title justify-center">Login page!</h2>
+          <div>
+            <div className="m-2">
+              <legend className="fieldset-legend"> EmailId</legend>
+              <input type="text" className="input" placeholder="" value={emailId} onChange={(e) => setEmailId(e.target.value)} />
             </div>
-            <button className="btn btn-primary" style={{marginTop:'30px'}}>Login</button>
+
+            <div className="m-2">
+              <legend className="fieldset-legend">Password</legend>
+              <input type="text" className="input" placeholder="" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+          </div>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" style={{ marginTop: "30px" }} onClick={handleLogin}>
+              Login
+            </button>
           </div>
         </div>
       </div>
